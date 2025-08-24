deca24fb9af8b9b412946f5a9bce8023eca300cc
Cosmosiai <akylbekbektenaliev@gmail.com>
2025-08-24 17:43:26 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import telebot
+print('hello world')
+
+bot = telebot.TeleBot("")
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(messege):
+    bot.send_message(messege.from_user.id, "I know what u wrote me"+str(messege.text))
+
+bot.polling(none_stop=True, interval=0)
